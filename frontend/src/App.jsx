869458e02e28570f6db07f8e40e61a0483735a10
Reem Mohamed <ream2004523farouk@gmail.com>
2026-04-17 17:00:16 +0200

    update

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      this is the IEEE team
+      <h1 class="text-3xl font-bold underline bg-amber-500">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
