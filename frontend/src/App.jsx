4bb087ab8c87e8c1647caa8ea6e7c7d57f80340e
Reem Mohamed <ream2004523farouk@gmail.com>
2026-04-17 20:33:52 +0200

    update frontend features and structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import { BrowserRouter } from 'react-router-dom'
+import AppRoutes from './app/routes/AppRoutes'
 export default function App() {
   return (
     <div>
-      this is the IEEE team
-      <h1 class="text-3xl font-bold underline bg-amber-500">
-        Hello world!
-      </h1>
+      <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
     </div>
   )
 }
