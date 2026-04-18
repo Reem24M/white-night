ed15527b9edcbea909ffcdc1bcaab7a0a7f070b9
Reem Mohamed <ream2004523farouk@gmail.com>
2026-04-18 20:58:53 +0200

    Update frontend UI and layouts v3

--- frontend/src/app/routes/AppRoutes.jsx
+++ frontend/src/app/routes/AppRoutes.jsx
@@ -3,24 +3,24 @@ import { Routes, Route } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import AuthLayout from "../layouts/AuthLayout";
 
-// لاحظ أسماء الملفات والمجلدات هنا
+
 import Home from "@/features/home/pages/home";
 import SearchPage from "@/features/search/pages/SearchPage";
-import HallDetails from "@/features/hall/pages/hallDetails";
-import BookingPage from "@/features/booking/pages/bookingPage"; 
+import HallDetails from "../../features/Hall/pages/hallDetails";
+import BookingPage from "../../features/booking/pages/bookingPage"; 
 
-import Login from "@/features/Auth/pages/login"; // التأكد من أن مجلد Auth يبدأ بحرف كبير
+import Login from "@/features/Auth/pages/login"; 
 import Register from "@/features/Auth/pages/register";
 
 import Favorites from "@/features/favorites/pages/Favorites";
-import Profile from "@/features/profile/pages/profile"; // التأكد من أنها صغيرة كما في الصورة
+import Profile from "@/features/profile/pages/profile"; 
 import About from "@/features/about/pages/about"; 
-import NotFound from "@/features/error"; // التأكد من اسم الملف هو error.jsx فعلاً
+import NotFound from "@/features/error"; 
 
 export default function AppRoutes() {
   return (
     <Routes>
-      {/* ✅ كل الصفحات اللي فيها Navbar + Footer */}
+     
       <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<SearchPage />} />
@@ -31,7 +31,7 @@ export default function AppRoutes() {
         <Route path="/profile" element={<Profile />} />
       </Route>
 
-      {/* ❌ صفحات Auth بدون Navbar/Footer */}
+      
       <Route element={<AuthLayout />}>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
