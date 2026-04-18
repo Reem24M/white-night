3c52f0be0339e553e72f2fbdf95c00b9e05483c6
hager <hagermahmod@gmail.com>
2026-04-18 13:57:53 +0200

    forgetpassword

--- app.js
+++ app.js
@@ -6,16 +6,11 @@ import cors from "cors";
 
 import  connectDB  from "./config/connectdb.js";
 import authRoutes from "./Routes/auth.route.js";
-// import userRoutes from "../backend/Routes/user.route.js";
-// import RestaurantsRoutes from "../backend/Routes/restaurant.route.js";
+import userRoutes from "./routes/user.route.js";
 import {
   errorHandler,
   notFoundHandler,
 } from "./middleware/notfounderrorhandler.js";
-// import SettingsRoutes from "../backend/Routes/adminSettings.route.js";
-// import favRestaurantsRoutes from "../backend/Routes/favRestaurants.route.js";
-// import ReviewsRoutes from "../backend/Routes/reviews.route.js";
-// import restaurantDataRoutes from "../backend/Routes/restaurantData.route.js";
 
 const app = express();
 app.use(cors());
@@ -23,22 +18,18 @@ app.use(json());
 
 await connectDB();
 
-// temporary route for check back response on vercel
-// app.get("/", (req, res) => {
-//   res.status(200).json({ message: "API is running smoothly on Vercel" });
-// });
 
 // // Routes
 app.use("/auth", authRoutes);
-// app.use("/user", userRoutes);
+app.use("/user", userRoutes);
 // app.use("/restaurants", RestaurantsRoutes);
 // app.use("/admin", SettingsRoutes);
 // app.use("/favorites", favRestaurantsRoutes);
 // app.use("/reviews", ReviewsRoutes);
 // app.use("/restaurant-data", restaurantDataRoutes);
 
-// app.use(notFoundHandler);
-// app.use(errorHandler);
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -3,10 +3,13 @@ const router=Router();
 
 import { login } from "../controller/login.controller.js";
 import { register } from "../controller/register.controller.js";
+import { forgotPassword, resetPassword } from "../controller/forgetPassword.js";
 
 
 router.post('/login', login);
 router.post('/register', register);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', resetPassword);
 
 
 
