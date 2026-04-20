78f2d6473db42c16437fb9df4a69c746ae202c95
hager <hagermahmod@gmail.com>
2026-04-20 13:28:33 +0200

    vercel

--- app.js
+++ app.js
@@ -1,10 +1,51 @@
+// import { configDotenv } from "dotenv";
+// configDotenv();
+
+// import express, { json } from "express";
+// import cors from "cors";
+
+// import  connectDB  from "./config/connectdb.js";
+// import authRoutes from "./routes/auth.route.js";
+// import userRoutes from "./routes/user.route.js";
+// import {
+//   errorHandler,
+//   notFoundHandler,
+// } from "./middleware/notfounderrorhandler.js";
+
+// const app = express();
+// app.use(cors());
+// app.use(json());
+
+// await connectDB();
+
+
+// // // Routes
+// app.use("/auth", authRoutes);
+// app.use("/user", userRoutes);
+// // app.use("/restaurants", RestaurantsRoutes);
+// // app.use("/admin", SettingsRoutes);
+// // app.use("/favorites", favRestaurantsRoutes);
+// // app.use("/reviews", ReviewsRoutes);
+// // app.use("/restaurant-data", restaurantDataRoutes);
+
+// app.use(notFoundHandler);
+// app.use(errorHandler);
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running locally on http://localhost:${PORT}`);
+// });
+// export default app;
+
+
+
 import { configDotenv } from "dotenv";
 configDotenv();
 
 import express, { json } from "express";
 import cors from "cors";
 
-import  connectDB  from "./config/connectdb.js";
+import connectDB from "./config/connectdb.js";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import {
@@ -16,23 +57,25 @@ const app = express();
 app.use(cors());
 app.use(json());
 
-await connectDB();
+connectDB(); 
 
+app.get("/", (req, res) => {
+  res.send("Server is running perfectly on Vercel!");
+});
 
-// // Routes
+// Routes
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
-// app.use("/restaurants", RestaurantsRoutes);
-// app.use("/admin", SettingsRoutes);
-// app.use("/favorites", favRestaurantsRoutes);
-// app.use("/reviews", ReviewsRoutes);
-// app.use("/restaurant-data", restaurantDataRoutes);
 
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running locally on http://localhost:${PORT}`);
-});
+// الـ listen دي بتنفعك بس وانتي شغالة Local
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server is running locally on http://localhost:${PORT}`);
+    });
+}
+
 export default app;
